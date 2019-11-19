ddbbbbb0ab4d2e009ff664450049c17e190cb276
Alex <apieschel@gmail.com>
2019-11-19 15:22:01 +0000

    🌓🏤 Checkpoint ./src/App.js:2265073/447

--- src/App.js
+++ src/App.js
@@ -36,11 +36,15 @@ class App extends Component {
     return (
       <div className='container'>
         <div className='grid'>
-            {this.state.cells.map((row,i)=><div key={i} className='row'>{row.map((cell)=><div key={cell.key} className='cell'></div>)}</div>)}
+            <Cells cells={this.state.cells} />
         </div>
       </div>
     );
   }
+}
+
+const Cells = function(props) {
+  return (props.cells.map((row,i)=><div key={i} className='row'>{row.map((cell)=><div key={cell.key} className='cell'></div>)}</div>) )
 }
 
 export default App;
