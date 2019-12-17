256861046c75414812a0552aea226ac38e28b5a7
Alex <apieschel@gmail.com>
2019-12-17 19:55:56 +0000

    🐿👻 Checkpoint ./src/App.js:2265073/2004

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const KICK = new Audio('https://cdn.glitch.com/17f54245-b142-4cf8-a81b-65e0b36f6b8f%2FMT52_bassdrum.wav?1551990664247');
+const AC = new AudioContext();
+const NODE = AC.createGain();
+
 class App extends Component {
   constructor() {
     super();
     
     this.state = {
+      audio: false,
       cells: [],
       cellsPerRow: 40,
       intervalId: 0,
@@ -19,12 +24,36 @@ class App extends Component {
   }
   
   componentDidMount() {
-    const intervalId = setInterval(this.updateGrid, 480);
+    const intervalId = setInterval(this.updateGrid, 120);
     
     // store intervalId in the state so it can be accessed later:
     this.setState({ intervalId: intervalId });
     
     this.buildGrid();
+  }
+  
+  playSound() {
+    let clone = KICK.cloneNode(true);
+    let buffer;
+      
+    const request = new XMLHttpRequest();
+    request.open('GET', KICK.src, true);
+    request.responseType = 'arraybuffer';
+    request.onload = function() {
+      AC.decodeAudioData(request.response, function(buffer) {
+        const gain = AC.createGain();
+        const playSound = AC.createBufferSource();
+        playSound.buffer = buffer;
+        playSound.connect(gain);
+        gain.connect(NODE);
+        gain.connect(AC.destination);
+        playSound.start(0);
+
+        clone.remove();
+      });     
+    }
+      
+      request.send();
   }
   
   buildGrid() {
@@ -45,6 +74,9 @@ class App extends Component {
     let grid = [];
     let cells = this.state.cells;
     
+    if(this.state.audio) {
+      KICK.play();
+    } 
     
     for(let i = 0; i < this.state.numberOfRows; i++) {
       grid.push([]);        
@@ -85,7 +117,7 @@ class App extends Component {
   
   handleClick(id) {
     this.setState({
-      cells: this.state.cells.map((row) => {
+        cells: this.state.cells.map((row) => {
            return row.map((cell) => {
              if(cell.key === id) {
                return Object.assign({}, cell, {
@@ -95,8 +127,10 @@ class App extends Component {
                return cell;
              }
            })
-      }),
-    });
+        }),
+        audio: true,
+      },
+    );
   }
   
   render() {
